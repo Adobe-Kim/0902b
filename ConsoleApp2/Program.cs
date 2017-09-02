9e5f8c7be4b9bccae423ca135f5209ef951ff308
DESKTOP-U80NJG0\adobe <kim@motionimaging.net>
2017-09-02 16:14:24 +0900

    call by value , call bt reference

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 3;
+            int y = 5;
+            int c = 0;
+
+            PrintXY(x, y);
+
+            Swap(x, y);                       // call by value
+            PrintXY(x, y);
+
+
+            c = Swap(ref x, ref y);               // call by reference
+            PrintXY(x, y);
+        }
+
+        static void Swap(int a, int b)
+        {
+            int c = 0;
+
+            c = a;
+            a = b;
+            b = c;
+        }
+
+        static int Swap(ref int a, ref int b)
+        {
+            int c = 0;
+
+            c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine("It's work");
+            return a + b;
+        }
+
+        static void PrintXY(int a, int b)
+        {
+            Console.Write("x=");
+            Console.WriteLine(a);
+            Console.Write("y=");
+            Console.WriteLine(b);
+            Console.WriteLine();
+        }
+
+
+    }
+}
